19a27b54ef4d00a2edf514fdb8442699e3f2c421
secretMoi <win.moi@hotmail.com>
2022-05-02 10:02:09 +0200

    Validation des beans

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -1,31 +1,38 @@
 package com.mycompany.invoise.invoiseweb.controller;
 
 
-import com.mycompany.invoise.core.controller.IInvoiceController;
 import com.mycompany.invoise.core.model.Invoice;
 import com.mycompany.invoise.core.service.IInvoiceService;
+import com.mycompany.invoise.invoiseweb.form.InvoiceForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/invoice")
-public class InvoiceControllerWeb implements IInvoiceController {
+public class InvoiceControllerWeb {
 
     @Autowired
     private IInvoiceService invoiceService;
 
-    public void setInvoiceService(IInvoiceService invoiceService) {
-        this.invoiceService = invoiceService;
-    }
-
     @PostMapping
-    public String createInvoice(@ModelAttribute Invoice invoice) {
+    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return "invoice-create-form";
+        }
+
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(invoiceForm.getCustomerName());
+        invoice.setOrderNumber(invoiceForm.getOrderNumber());
 
         invoiceService.createInvoice(invoice);
 
@@ -49,7 +56,7 @@ public class InvoiceControllerWeb implements IInvoiceController {
     }
 
     @GetMapping("/create-form")
-    public String displayInvoiceCreateForm(@ModelAttribute Invoice invoice) {
+    public String displayInvoiceCreateForm(@ModelAttribute InvoiceForm invoice) {
         return "invoice-create-form";
     }
 }
