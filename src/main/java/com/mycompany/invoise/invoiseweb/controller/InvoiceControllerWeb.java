8cb4cdd7654cf7f02a793b155208055393ea5ab0
secretMoi <win.moi@hotmail.com>
2022-05-06 10:56:53 +0200

    ResponseBody, RequestBody et RestController

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -6,17 +6,18 @@ import com.mycompany.invoise.core.service.IInvoiceService;
 import com.mycompany.invoise.invoiseweb.form.InvoiceForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/invoice")
 public class InvoiceControllerWeb {
 
@@ -24,35 +25,21 @@ public class InvoiceControllerWeb {
     private IInvoiceService invoiceService;
 
     @PostMapping
-    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult bindingResult) {
+    public Invoice create(@RequestBody Invoice invoice) {
 
-        if(bindingResult.hasErrors()) {
-            return "invoice-create-form";
-        }
-
-        Invoice invoice = new Invoice();
-        invoice.setCustomerName(invoiceForm.getCustomerName());
-        invoice.setOrderNumber(invoiceForm.getOrderNumber());
-
-        invoiceService.createInvoice(invoice);
-
-        return "invoice-created";
+        return invoiceService.createInvoice(invoice);
     }
 
-    @GetMapping("/home")
-    public String displayHome(Model model) {
+    @GetMapping
+    public List<Invoice> list() {
 
-        model.addAttribute("invoices", invoiceService.getInvoiceList());
-
-        return "invoice-home";
+        return invoiceService.getInvoiceList();
     }
 
     @GetMapping("/{id}")
-    public String displayInvoice(@PathVariable("id") String number, Model model) {
+    public Invoice get(@PathVariable("id") String number) {
 
-        model.addAttribute("invoice", invoiceService.getInvoiceByNumber(number));
-
-        return "invoice-details";
+        return invoiceService.getInvoiceByNumber(number);
     }
 
     @GetMapping("/create-form")
